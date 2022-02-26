ef7a8a26f9fb8aa2d8a0fdc776dd60997de012dc
Dawid Lachowicz <domdaw7@gmail.com>
2022-02-26 15:16:48 +0000

    Add input validation

--- app/controllers/company_searches_controller.rb
+++ app/controllers/company_searches_controller.rb
@@ -1,22 +1,48 @@
 class CompanySearchesController < ApplicationController
   def new
+    @errors = {}
   end
 
   def show
-    # TODO validate input
-    # TODO remember the category once selected
+    @errors = {}
+
+    # TODO move to config
     Google::Maps.configure do |config|
       config.authentication_mode = Google::Maps::Configuration::API_KEY
       config.api_key = Rails.application.credentials.gmaps_api_key
     end
-    result = Google::Maps.geocode(params[:location])
-    @lat = result.first.latitude
-    @long = result.first.longitude
-    @radius = params[:radius]
+
+    if params[:location] == ""
+      @errors["location"] = "Please fill in the location field."
+    else
+      result = Google::Maps.geocode(params[:location])
+      if result && result.first
+        @lat = result.first.latitude
+        @long = result.first.longitude
+      else
+        @errors["location"] = "Unable to find entered location. Please try again."
+      end
+    end
+
+    if params[:radius] == ""
+      @radius = 1
+    else
+      @radius = params[:radius]
+    end
     @location_name = params[:location]
 
-    @category_id = params[:category_id]
-    @category = Category.find(@category_id)
-    @company_list = Company.where(category: @category_id).order(rating: :desc)
+    if params[:category_id] == ""
+      @errors["category"] = "Please select the sector."
+    else
+      @category_id = params[:category_id]
+      @category = Category.find(@category_id)
+      @company_list = Company.where(category: @category_id).order(rating: :desc)
+    end
+
+    if @errors.length > 0
+      @lat = nil
+      @long = nil
+      render 'new'
+    end
   end
 end
--- app/helpers/company_searches_helper.rb
+++ app/helpers/company_searches_helper.rb
@@ -1,2 +1,9 @@
 module CompanySearchesHelper
+  def error_element(error_type)
+    if @errors[error_type]
+      content_tag(:div, class: "error", id: "#{error_type}-error") do
+        @errors[error_type]
+      end
+    end
+  end
 end
